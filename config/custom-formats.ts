f28c16f637c9d4927580ce2c42c846154e2ea32e
Unity123 <38165550+Unity123@users.noreply.github.com>
2024-08-09 20:57:57 -0600

    add RUstricted

---
+++ config/custom-formats.ts
@@ -0,0 +1,14 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+  {
+    section: "Gaysifleur",
+  },
+  {
+    name: "[Gen 9] RUstricted",
+    mod: 'gen9',
+    ruleset: ['[Gen 9] ZU', 'Limit One Restricted'],
+    restricted: ['RU'],
+  }
+];
