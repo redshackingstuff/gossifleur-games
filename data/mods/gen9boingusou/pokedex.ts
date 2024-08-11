15bb82d4b8acd66c489da5b7f01429c6f554343a
0neWingedAngel <lampshadehatwearer@gmail.com>
2024-08-11 18:16:38 -0400

    Arboliva-Mega

--- data/mods/gen9boingusou/pokedex.ts
+++ data/mods/gen9boingusou/pokedex.ts
@@ -1,5 +1,24 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
-  delibird: {
+	arboliva: {
+			inherit: true,
+			otherFormes: ["Arboliva-Mega"],
+		formeOrder: ["Arboliva", "Arboliva-Mega"]
+	},
+	arbolivamega: {
+		num: 930,
+		name: "Arboliva-Mega",
+		baseSpecies: "Arboliva",
+		forme: "Mega",
+		types: ["Grass", "Fire"],
+		baseStats: {hp: 78, atk: 84, def: 125, spa: 140, spd: 116, spe: 67},
+		abilities: {0: "Grassy Surge"},
+		heightm: 1.6,
+		weightkg: 55.4,
+		color: "Red",
+		eggGroups: ["Grass"],
+		requiredItem: "Arbolivite",
+	},  
+	delibird: {
 		inherit: true,
 		otherFormes: ["Delibird-Mega"],
     formeOrder: ["Delibird", "Delibird-Mega"],
