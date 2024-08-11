09889ff776d36aac77e3595139e8981b3590d18a
Unity123 <38165550+Unity123@users.noreply.github.com>
2024-08-11 11:14:54 -0600

    make the code be in the right spot

--- data/mods/gen9boingusou/scripts.ts
+++ data/mods/gen9boingusou/scripts.ts
@@ -1,10 +1,16 @@
 // making hyper beam act like in gen 1 (copied right from the gen 1 code on showdown)
-// If target fainted
-if (target && target.hp <= 0) {
-// We remove recharge
- if (pokemon.volatiles['mustrecharge']) pokemon.removeVolatile('mustrecharge');
-		delete pokemon.volatiles['partialtrappinglock'];
-} else {
-	      if (pokemon.volatiles['mustrecharge']) this.battle.add('-mustrecharge', pokemon);
+export const Scripts: ModdedBattleScriptsData = {
+	actions: {
+		useMove(moveOrMoveName, pokemon, target, sourceEffect) {
+			// If target fainted
+			if (target && target.hp <= 0) {
+				// We remove recharge
+ 				if (pokemon.volatiles['mustrecharge']) pokemon.removeVolatile('mustrecharge');
+				delete pokemon.volatiles['partialtrappinglock'];
+			} else {
+	      	if (pokemon.volatiles['mustrecharge']) this.battle.add('-mustrecharge', pokemon);
 				if (pokemon.hp) this.battle.runEvent('AfterMoveSelf', pokemon, target, move);
-					}
+			}
+		}
+	}
+}
