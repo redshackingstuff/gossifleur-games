0394709389369594b06767a2c1f21304305743ca
Unity123 <38165550+Unity123@users.noreply.github.com>
2024-08-10 19:56:43 -0600

    same with mega delibird

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -4464,21 +4464,7 @@ export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
 		color: "Red",
 		eggGroups: ["Water 1", "Field"],
 	},
-		delibirdmega: {
-		num: 225,
-		name: "Delibird-Mega",
-		baseSpecies: "Delibird",
-		forme: "Mega",
-		types: ["Ice", "Flying"],
-		baseStats: {hp: 45, atk: 105, def: 45, spa: 65, spd: 45, spe: 125},
-		abilities: {0: "Huge Power"},
-		heightm: 0.9,
-		weightkg: 16,
-		color: "Red",
-		eggGroups: ["Water 1", "Field"],
-		requiredItem: "Delibirdite",
-	},
-		mantine: {
+	mantine: {
 		num: 226,
 		name: "Mantine",
 		types: ["Water", "Flying"],
