ed117ced680e0ddd210d59658603ccf0d953e0b5
0neWingedAngel <lampshadehatwearer@gmail.com>
2024-08-11 18:25:01 -0400

    Arbolivite

--- data/mods/gen9boingusou/items.ts
+++ data/mods/gen9boingusou/items.ts
@@ -1,5 +1,18 @@
 export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
-  delibirdite: {
+    delibirdite: {
+		name: "Arbolivite",
+		spritenum: 41,
+		megaStone: "Arboliva-Mega",
+		megaEvolves: "Arboliva",
+		itemUser: ["Arboliva"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 1887,
+		gen: 9,
+	 },
+	delibirdite: {
 		name: "Delibirdite",
 		spritenum: 41,
 		megaStone: "Delibird-Mega",
@@ -9,7 +22,7 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
 			return true;
 		},
-		num: 1887,
+		num: 1888,
 		gen: 9,
 	}
 }
