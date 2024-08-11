34a375d7966824d9a9aae7b672afb3b36bd496a0
solrock-da-coder <gorb4758@gmail.com>
2024-08-11 16:16:15 -0400

    Create abilites.ts
    new abilities for the new megas

---
+++ data/mods/gen9boingusou/abilites.ts
@@ -0,0 +1,29 @@
+// bring other abilites not in this file
+export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
+// solarize (mega solrock's ability)
+solarize: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) &&
+				!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Fire';
+        // is this what boosts moves affected by solarize? idk
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+  // idk wtf onBasePowerPriority is, so I ain't changing it
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+      //same with this line
+			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
+		},
+		flags: {},
+		name: "Solarize",
+  // do ratings ever even get used?
+		rating: 4,
+		num: 310,
+	},
+},
