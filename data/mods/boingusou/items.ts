402979dc21bc0a44ab4620bc6706c70ad4814b99
Unity123 <38165550+Unity123@users.noreply.github.com>
2024-08-10 19:55:08 -0600

    Create items.ts

---
+++ data/mods/boingusou/items.ts
@@ -0,0 +1,15 @@
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+  delibirdite: {
+		name: "Delibirdite",
+		spritenum: 41,
+		megaStone: "Delibird-Mega",
+		megaEvolves: "Delibird",
+		itemUser: ["Delibird"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true;
+		},
+		num: 1887,
+		gen: 9,
+	}
+}
