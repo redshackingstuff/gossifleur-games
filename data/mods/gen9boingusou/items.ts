2f1ac3e39cbf987a31f5a8287d197328d92c6eed
solrock-da-coder <gorb4758@gmail.com>
2024-08-12 15:49:58 -0400

    Update items.ts
    adds solrockite

--- data/mods/gen9boingusou/items.ts
+++ data/mods/gen9boingusou/items.ts
@@ -11,5 +11,18 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		},
 		num: 1887,
 		gen: 9,
-	}
+	},
+	solrockite: {
+		name: "Solrockite",
+		sprtienum: 41,
+		megaStone: "Solrock-Mega",
+		MegaEvolves: "Solrock",
+		itemUser: ["Solrock"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
+			return true
+		},
+		num: 1889,
+		gen: 9,
+	},
 }
