f59fc4876470bfaa897da8408738b5210b2f4083
Nicidical <radicalryan75@gmail.com>
2024-08-12 12:48:02 -0400

    mega breloom added to pokedex

--- data/mods/gen9boingusou/pokedex.ts
+++ data/mods/gen9boingusou/pokedex.ts
@@ -17,5 +17,24 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 		color: "Red",
 		eggGroups: ["Water 1", "Field"],
 		requiredItem: "Delibirdite",
+	},
+	breloom: {
+		inherit: true,
+		otherFormes: ["Breloom-Mega"],
+    formeOrder: ["Breloom", "Breloom-Mega"],
+  },
+	breloommega: {
+		num: 286,
+		name: "Breloom-Mega",
+		baseSpecies: "Breloom",
+		forme: "Mega",
+		types: ["Grass", "Fighting"],
+		baseStats: {hp: 60, atk: 150, def: 120, spa: 50, spd: 110, spe: 60},
+		abilities: {0: "Technician"}, // Place holder until Fungal Touch is implemented
+		heightm: 1.2,
+		weightkg: 39.2,
+		color: "Green",
+		eggGroups: ["Fairy", "Grass"],
+		requiredItem: "Breloomite",
 	}
 }
