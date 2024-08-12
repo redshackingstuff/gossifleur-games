b9ab6173874503d0339d951ece8945fe8e1ecd5d
Unity123 <38165550+Unity123@users.noreply.github.com>
2024-08-11 19:11:22 -0600

    remove the comma at the end

--- data/mods/gen9boingusou/abilites.ts
+++ data/mods/gen9boingusou/abilites.ts
@@ -1,7 +1,7 @@
 // bring other abilites not in this file
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
-// solarize (mega solrock's ability)
-solarize: {
+	// solarize (mega solrock's ability)
+	solarize: {
 		onModifyTypePriority: -1,
 		onModifyType(move, pokemon) {
 			const noModifyType = [
@@ -14,16 +14,16 @@ solarize: {
 				move.typeChangerBoosted = this.effect;
 			}
 		},
-  // idk wtf onBasePowerPriority is, so I ain't changing it
+ 		// idk wtf onBasePowerPriority is, so I ain't changing it
 		onBasePowerPriority: 23,
 		onBasePower(basePower, pokemon, target, move) {
-      //same with this line
+      	//same with this line
 			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
 		},
 		flags: {},
 		name: "Solarize",
-  // do ratings ever even get used?
+		// do ratings ever even get used?
 		rating: 4,
 		num: 310,
 	},
-},
+}
