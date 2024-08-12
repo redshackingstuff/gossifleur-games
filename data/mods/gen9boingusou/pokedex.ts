223f1494a6cc08406cc8cba3c4bf0551d2dc8da0
solrock-da-coder <gorb4758@gmail.com>
2024-08-12 15:33:01 -0400

    Update pokedex.ts
    adds mega solrock

--- data/mods/gen9boingusou/pokedex.ts
+++ data/mods/gen9boingusou/pokedex.ts
@@ -17,5 +17,23 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 		color: "Red",
 		eggGroups: ["Water 1", "Field"],
 		requiredItem: "Delibirdite",
-	}
+	},
+	solrock: {
+		inherit: true,
+			otherFormes: ["Solrock-Mega"],
+			formeOrder: ["Solrock","Solrock-Mega"]
+	},
+	solrockmega: {
+		num: 338,
+		name: "Solrock-Mega",
+		baseSpecies: "Solrock",
+		forme: "Mega",
+		types: ["Rock", "Fire"],
+		baseStats: {hp: 90, atk: 120, def: 110, spa: 55, spd: 85, spe: 105},
+		abilities: {0: "Solarize"},
+		heightm: 1.5,
+		weightkg: 200,
+		color: "Red",
+		eggGroups: ["Mineral"]
+	},
 }
