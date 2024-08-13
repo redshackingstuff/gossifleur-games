fb79ef8c21c551460825e7f3d1355d1c8b792334
Nicidical <radicalryan75@gmail.com>
2024-08-13 11:13:29 -0400

    added semicolon :3

--- data/mods/gen9boingusou/abilites.ts
+++ data/mods/gen9boingusou/abilites.ts
@@ -6,7 +6,7 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 			// If a move is a contact move without draining effect, have it drain 1/3 of damage dealt
 			// If a move is a draining move, apply a big root effect, increasing drained damage by 30%
 			if (move.drain === undefined && move.flags['contact']) { move.drain = [1, 3]; }
-			else if (!move.drain === undefined) { move.drain[0] *= 13; move.drain[1] *= 10 }
+			else if (!move.drain === undefined) { move.drain[0] *= 13; move.drain[1] *= 10; }
 		},
 		flags: {},
 		name: "Fungal Touch",
