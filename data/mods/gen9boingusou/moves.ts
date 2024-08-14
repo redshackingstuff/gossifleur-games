d2147b52f3fb8d0c8e3a624a4f12d725e6646985
Nicidical <radicalryan75@gmail.com>
2024-08-14 18:01:18 -0400

    I think I figured it out :D

--- data/mods/gen9boingusou/moves.ts
+++ data/mods/gen9boingusou/moves.ts
@@ -1,4 +1,4 @@
-export const Moves: import('../sim/dex-moves').MoveDataTable = {
+export const Moves: import('../../../sim/dex-moves').MoveDataTable = {
 	fungaljab: {
 		num: 920,
 		accuracy: 100,
@@ -8,7 +8,12 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, punch: 1, heal: 1, metronome: 1},
-		drain: [1, 2], // need to figure out how to increase drain to [3, 4] on statused/comatose opponent
+		onModifyMove(move, pokemon, target) {
+			if (target && (target.status === 'psn' || target.status === 'tox' || target.status === 'prz'  || target.status === 'slp' || target.hasAbility('comatose'))) {
+				move.drain = [3, 4];
+			}
+		},
+		drain: [1, 2],
 		secondary: null,
 		target: "normal",
 		type: "Grass",
