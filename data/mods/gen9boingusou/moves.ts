d85fb4b81344721901603609a4295e89fde1cd5f
solrock-da-coder <gorb4758@gmail.com>
2024-08-14 17:18:49 -0400

    Update moves.ts
    lol, forgot phys on some of them

--- data/mods/gen9boingusou/moves.ts
+++ data/mods/gen9boingusou/moves.ts
@@ -37,7 +37,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		type: "Bug",
 		contestType: "Clever",
 	},
-	hiddenpowerdark: {
+	physhiddenpowerdark: {
 		num: 1001,
 		accuracy: 100,
 		basePower: 60,
@@ -133,7 +133,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		type: "Flying",
 		contestType: "Clever",
 	},
-	hiddenpowerghost: {
+	physhiddenpowerghost: {
 		num: 237,
 		accuracy: 100,
 		basePower: 60,
