ed75021d080cec29ebc4afe8ab29c8f57f02229b
Nicidical <radicalryan75@gmail.com>
2024-08-14 20:20:16 -0400

    reordering drain just in case

--- data/mods/gen9boingusou/moves.ts
+++ data/mods/gen9boingusou/moves.ts
@@ -7,13 +7,13 @@ export const Moves: import('../../../sim/dex-moves').MoveDataTable = {
 		name: "Fungal Jab",
 		pp: 10,
 		priority: 0,
+		drain: [1, 2],
 		flags: {contact: 1, protect: 1, mirror: 1, punch: 1, heal: 1, metronome: 1},
 		onModifyMove(move, pokemon, target) {
 			if (target && (target.status === 'psn' || target.status === 'tox' || target.status === 'prz'  || target.status === 'slp' || target.hasAbility('comatose'))) {
 				move.drain = [3, 4];
 			}
 		},
-		drain: [1, 2],
 		secondary: null,
 		target: "normal",
 		type: "Grass",
