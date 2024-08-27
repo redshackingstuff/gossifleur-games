2bbbc92e9ed544060b97762cac7f6d34e031f72c
Lilian Bazantay <lilian.bazantay@epitech.eu>
2024-08-27 16:05:59 +0200

    abilities.ts: add spectriate

--- data/mods/gen9boingusou/abilites.ts
+++ data/mods/gen9boingusou/abilites.ts
@@ -121,6 +121,31 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		num: 311,
   },
 
+  	// spectriate (mega greninja's ability)
+	spectriate: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) &&
+				!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Ghost';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+ 		// idk wtf onBasePowerPriority is, so I ain't changing it
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
+		},
+		flags: {},
+		name: "Spectriate",
+		// do ratings ever even get used?
+		rating: 4,
+		num: 316,
+  },
+
   lifesteal: {
 
 		onModifyMove(move) {
